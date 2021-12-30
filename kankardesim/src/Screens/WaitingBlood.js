7b1188f7e58055ddbf5b268e97157938678d684f
Oguzhan <oguzhanbalta99@gmail.com>
2021-12-30 16:01:51 +0300

    telefon çağrı yönlendirmesi yapıldı.

--- kankardesim/src/Screens/WaitingBlood.js
+++ kankardesim/src/Screens/WaitingBlood.js
@@ -1,72 +1,94 @@
-import React from "react";
-import { View, Text, ImageBackground, TouchableNativeFeedback, Image, StyleSheet, Dimensions, ScrollView } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import HomeHeader from "../Components/HomeHeader";
+import React, { useState, useEffect } from "react";
+import { View, Text, ImageBackground, Linking, StyleSheet, Dimensions, ScrollView } from "react-native";
+import { Card, Icon, } from "react-native-elements";
 import SearchComponent from "../Components/SearchComponent";
-import { kanBekleyenData, kanGruplariData, ilcelerData } from "../Global/data";
 import { colors } from "../Global/styles";
-import WaitingBloodCard from "../Components/WaitingBloodCard";
-import { isTemplateElement } from "@babel/types";
 
-
+import firestore from "@react-native-firebase/firestore";
+import { TouchableOpacity } from "react-native-gesture-handler";
 const SCREEN_WIDTH = Dimensions.get('window').width
-const imageCard = { uri: "https://www.nawpic.com/media/2020/red-background-nawpic.png" }
 
 export default function WaitingBlood({ navigation }) {
+
+    const [ilanlar, setIlanlar] = useState([])
+
+    const fetchIlanlar = async () => {
+        const ilanlarCollection = await firestore().collection('ilanlar').get()
+        console.log(ilanlarCollection.docs)
+        setIlanlar(
+            ilanlarCollection.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id }
+            })
+        )
+    }
+
+    const deleteIlan = async (id) => {
+        const res = await firestore().collection('ilanlar').doc(id).delete()
+        console.log(res)
+        fetchIlanlar()
+    }
+
+
+    useEffect(() => {
+        fetchIlanlar()
+        firestore().collection('students').where("type", "==", "ilan").onSnapshot(querySnapshot => {
+            querySnapshot.docChanges().forEach(change => {
+                if (change.type == 'added') {
+                    console.log('New ilan:', change.doc.data())
+                }
+                if (change.type == 'modified') {
+                    console.log('Modified ilan:', change.doc.data())
+                }
+                if (change.type == 'removed') {
+                    console.log('Removed ilan:', change.doc.data())
+                }
+                fetchIlanlar
+            })
+        })
+
+    }, [])
+
+
 
     return (
         <ImageBackground source={require('../Assets/image/back.png')} style={{ flex: 1 }}>
             <View style={{}}>
                 <SearchComponent />
-                <View style={styles.listHeader}>
-                    <Text style={{ fontSize: 16, color: "white", fontWeight: "bold" }}>Kan Grupları</Text>
-                </View>
-                <FlatList
-                    style={{ marginTop: 10, marginBottom: 10 }}
-                    horizontal={true}
-                    data={kanGruplariData}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item }) => (
-                        <TouchableNativeFeedback
-                            onPress={() => {
-                                navigation.navigate("SearchResultScreen", { item: item.name });
-                            }}
-                        >
-                            <View style={styles.imageView}>
+                <ScrollView>
+                    {
+                        ilanlar.map(ilan => {
+                            return (
+                                <Card key={ilan.id}>
+                                    <Card.Title style={{ fontSize: 16, color: colors.buttons }}>{ilan.isim} - {ilan.kan_grubu}</Card.Title>
+                                    <Card.Divider />
+                                    <Card.Title style={{ fontSize: 13, color: "black" }}>{ilan.hastane}/{ilan.ilce}/{ilan.sehir}</Card.Title>
+                                    <Card.Title style={{ fontSize: 13, color: "black" }}>{ilan.aciklama} - {ilan.tel_no}</Card.Title>
+                                    <Card.Divider />
+                                    <View style={{ flexDirection: 'row', justifyContent: "space-around" }}>
+                                        <Icon
+                                            name="call"
+                                            type="Ionicons"
+                                            color={"black"}
+                                            size={20}
+                                            onPress={()=>{
+                                                let phoneNumber=ilan.tel_no;
+                                                console.log(phoneNumber);
+                                                Linking.openURL(`tel:${phoneNumber}`);
+                                            }}
 
-                                <View style={styles.card}>
-                                    <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>{item.name}</Text>
-                                </View>
-                            </View>
+                                        />
 
-                        </TouchableNativeFeedback>
-                    )}
-                />
-                <View style={styles.listHeader}>
-                    <Text style={{ fontSize: 15, color: "white", fontWeight: "bold" }}>İlçeler</Text>
-                </View>
-                <FlatList
-                    style={{ marginTop: 10, marginBottom: 10 }}
-                    horizontal={true}
-                    data={ilcelerData}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item }) => (
-                        <TouchableNativeFeedback
-                            onPress={() => {
-                                navigation.navigate("SearchResultScreen", { item: item.name });
-                            }}
-                        >
-                            <View style={styles.imageView}>
 
-                                <View style={styles.card}>
-                                    <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>{item.name}</Text>
-                                </View>
-                            </View>
 
-                        </TouchableNativeFeedback>
-                    )}
-                />
-                
+
+                                    </View>
+                                </Card>
+                            )
+                        })
+                    }
+                </ScrollView>
+
+
             </View>
         </ImageBackground>
 
@@ -99,15 +121,15 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     listHeader: {
-        backgroundColor:'rgba(0, 0, 0, 0.4)',
+        backgroundColor: 'rgba(0, 0, 0, 0.4)',
         fontSize: 16,
         color: "white",
-        padding:15,
-        marginLeft:15,
-        marginTop:10,
-        marginRight:15,
-        borderRadius:30,
-        
+        padding: 15,
+        marginLeft: 15,
+        marginTop: 10,
+        marginRight: 15,
+        borderRadius: 30,
+
     },
     textView: {
         height: SCREEN_WIDTH * 0.4475,
